1f52ff308c5c24652eeb53c7d4083ae59ebcb4ac
ByAlphas <86494759+ByAlphas@users.noreply.github.com>
2024-03-04 18:31:29 +0300

    Add files via upload

---
+++ alpha.js
@@ -0,0 +1,37 @@
+function generatePassword() {
+    var length = document.getElementById("length").value;
+    var charset = "";
+    var password = "";
+
+    if (document.getElementById("lowercase").checked) {
+        charset += "abcdefghijklmnopqrstuvwxyz";
+    }
+
+    if (document.getElementById("uppercase").checked) {
+        charset += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    }
+
+    if (document.getElementById("numbers").checked) {
+        charset += "0123456789";
+    }
+
+    if (document.getElementById("specialChars").checked) {
+        charset += "!@#$%^&*()-_+=<>?/";
+    }
+
+    if (charset === "") {
+        alert("En az bir karakter kümesi seçilmelidir.");
+        return;
+    }
+
+    for (var i = 0; i < length; i++) {
+        var randomIndex = Math.floor(Math.random() * charset.length);
+        password += charset[randomIndex];
+    }
+
+    document.getElementById("password").value = password;
+}
+
+function updateLength(value) {
+    document.getElementById("length-display").innerText = value;
+}
